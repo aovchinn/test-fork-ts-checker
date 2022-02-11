18e11c37347780ab5090e44277d21074c41b925a
Alexander Ovchinnikov <demons94@ya.ru>
2022-02-11 20:26:49 +0300

    Delete test-fork-ts-checker directory

--- test-fork-ts-checker/app/index.ts
+++
@@ -1,3 +0,0 @@
-import * as mySharedStuff from "my-shared-stuff/index";
-
-console.log("Hi", mySharedStuff.a, mySharedStuff.b);
--- test-fork-ts-checker/my-shared-stuff/index.ts
+++
@@ -1,7 +0,0 @@
-let a = 1;
-a = "asd";
-export { a };
-
-let b = 1;
-b = "bsd";
-export { b };
